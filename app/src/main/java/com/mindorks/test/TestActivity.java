1009ff1379db703a0406ad0de17fcbfba10f04a8
janishar <janishar.ali@gmail.com>
2017-08-09 00:54:06 +0530

    Add EPHV expand-collapse, optimize imports

--- app/src/main/java/com/mindorks/test/TestActivity.java
+++ app/src/main/java/com/mindorks/test/TestActivity.java
@@ -1,32 +1,23 @@
 package com.mindorks.test;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.mindorks.butterknifelite.ButterKnifeLite;
 import com.mindorks.butterknifelite.annotations.BindView;
+import com.mindorks.butterknifelite.annotations.OnClick;
 import com.mindorks.placeholderview.ExpandablePlaceHolderView;
-import com.mindorks.placeholderview.PlaceHolderView;
-import com.mindorks.test.drawer.DrawerHeader;
-import com.mindorks.test.drawer.DrawerMenuItem;
 import com.mindorks.test.expandable.ChildItem;
 import com.mindorks.test.expandable.ParentItem;
-import com.mindorks.test.gallery.ImageTypeBig;
-import com.mindorks.test.gallery.ImageTypeSmallPlaceHolder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class TestActivity extends AppCompatActivity {
 
+    ParentItem parentItem;
     @BindView(R.id.toolbar)
     private Toolbar mToolbar;
-
     @BindView(R.id.expandableView)
     private ExpandablePlaceHolderView mExpandableView;
 
@@ -36,6 +27,8 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
         ButterKnifeLite.bind(this);
 
+        parentItem = new ParentItem(this.getApplicationContext());
+
         mExpandableView
                 .addView(new ParentItem(this.getApplicationContext()))
                 .addView(new ChildItem(mExpandableView))
@@ -43,7 +36,7 @@ public class TestActivity extends AppCompatActivity {
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView))
-                .addView(new ParentItem(this.getApplicationContext()))
+                .addView(parentItem)
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView))
@@ -83,5 +76,41 @@ public class TestActivity extends AppCompatActivity {
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView))
                 .addView(new ChildItem(mExpandableView));
+    }
+
+//    @OnClick(R.id.collapse)
+//    void onCollapse() {
+//        try {
+//            mExpandableView.collapse(0);
+//        }catch (Resources.NotFoundException e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @OnClick(R.id.expand)
+//    void onExpand() {
+//        try {
+//            mExpandableView.expand(0);
+//        }catch (Resources.NotFoundException e){
+//            e.printStackTrace();
+//        }
+//    }
+
+    @OnClick(R.id.collapse)
+    void onCollapse() {
+        try {
+            mExpandableView.collapse(parentItem);
+        } catch (Resources.NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.expand)
+    void onExpand() {
+        try {
+            mExpandableView.expand(parentItem);
+        } catch (Resources.NotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
