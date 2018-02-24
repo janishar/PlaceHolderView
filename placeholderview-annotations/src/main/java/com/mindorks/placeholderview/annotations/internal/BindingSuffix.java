25f799734372b514e2d4bd1fc42acd4f58cb571e
Ali <janishar.ali@gmail.com>
2018-02-24 17:01:57 +0530

    Add Generated Class Instantiation

---
+++ placeholderview-annotations/src/main/java/com/mindorks/placeholderview/annotations/internal/BindingSuffix.java
@@ -0,0 +1,17 @@
+package com.mindorks.placeholderview.annotations.internal;
+
+/**
+ * Created by janisharali on 24/02/18.
+ */
+
+public class BindingSuffix {
+
+    public static final String CLASS_VIEW_BINDER_SUFFIX = "$View_Binder";
+    public static final String CLASS_EXPANDABLE_VIEW_BINDER_SUFFIX = "$Expandable_View_Binder";
+    public static final String CLASS_SWIPE_VIEW_BINDER_SUFFIX = "$Swipe_View_Binder";
+    public static final String CLASS_SWIPE_DIRECTIONAL_VIEW_BINDER_SUFFIX = "$Directional_View_Binder";
+
+    private BindingSuffix() {
+        // not to be instantiated in public
+    }
+}
